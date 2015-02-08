1d2e7122e6667422488cf3c5e77dfab70daa819e
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-02-08 16:06:00 +0100

    Data is now stored

--- pysoft/parser.py
+++ pysoft/parser.py
@@ -1,9 +1,41 @@
 import gzip
 
 
+class Column(object):
+	def __init__(self, name, desc):
+		self.name = name
+		self.description = desc
+
+	def __repr__(self):
+		return self.name
+
+class Row(object):
+	""" Allow data access via column name and column index
+	"""
+
+	def __init__(self, col_names, row):
+		self.dict = {col: row[i] for i, col in enumerate(col_names)}
+		self.list = row
+
+	def __getitem__(self, key):
+		if type(key) == type(42):
+			return self.list[key]
+		elif type(key) == type('foo'):
+			return self.dict[key]
+		else:
+			raise TypeError('Only int and string allowed for indexing')
+
+	def __iter__(self):
+		return iter(self.list)
+
+	def __len__(self):
+		return len(self.list)
+
 class SOFTFile(object):
-	def __init__(self, fname):
+	def __init__(self, fname, skip_data=False):
 		self.header = {}
+		self.columns = []
+		self.data = []
 
 		if fname.endswith('.gz'):
 			with gzip.open(fname, 'rb') as fd:
@@ -12,11 +44,12 @@ class SOFTFile(object):
 			with open(fname, 'r') as fd:
 				self.content = fd.read()
 
-		self.parse()
+		self.parse(skip_data)
 
-	def parse(self):
+	def parse(self, skip_data):
 		section = None
 		dataset = None
+		get_kv = lambda f: (f[0].strip()[1:], f[1].strip())
 
 		for line in self.content.split('\n'):
 			fields = line.split('=')
@@ -37,14 +70,24 @@ class SOFTFile(object):
 					self.header[section] = {}
 			elif line.startswith('!'):
 				if '=' in line:
-					key = fields[0].strip()[1:].lower()
-					value = fields[1].strip()
+					key, value = get_kv(fields)
 
 					if section == 'subset':
 						access = self.header['dataset']['subsets'][-1]
 					else:
 						access = self.header[section]
 
-					access[key] = value
+					access[key.lower()] = value
+			elif line.startswith('#'):
+				name, desc = get_kv(fields)
+
+				self.columns.append(Column(name, desc))
 			else:
-				break
+				if skip_data: break
+
+				cols = [c.name for c in self.columns]
+				row = line.split('\t')
+				if len(row) != len(cols): continue
+				if row[0] == self.columns[0].name: continue # table header
+
+				self.data.append(Row(cols, row))
--- pysoft/tests/test_parser.py
+++ pysoft/tests/test_parser.py
@@ -21,7 +21,29 @@ class TestSOFTFileHeader(TestCase):
         self.assertEqual(len(self.soft.header['dataset']['subsets']), 6)
         self.assertEqual(self.soft.header['dataset']['subsets'][0]['subset_sample_id'], 'GSM37063,GSM37064,GSM37065,GSM37066,GSM37067')
 
+        self.assertEqual(len(self.soft.columns), 37)
+        self.assertEqual(self.soft.columns[0].name, 'ID_REF')
+        self.assertEqual(self.soft.columns[0].description, 'Platform reference identifier')
+
+        self.assertEqual(len(self.soft.data), 7312)
+        self.assertEqual(self.soft.data[0][0], self.soft.data[0]['ID_REF'])
+        self.assertEqual(self.soft.data[0][1], self.soft.data[0]['IDENTIFIER'])
+        self.assertEqual(self.soft.data[0][-1], self.soft.data[0]['GO:Component ID'])
+
+    def test_exceptions(self):
+        with self.assertRaises(TypeError):
+            self.soft.data[0][3.141]
+
     def test_gzipped_file(self):
         gz_soft = SOFTFile(os.path.join(self.data_dir, 'GDS1099.soft.gz'))
 
         self.assertEqual(gz_soft.header, self.soft.header)
+
+        for gzc, c in zip(gz_soft.columns, self.soft.columns):
+            self.assertEqual(gzc.name, c.name)
+            self.assertEqual(gzc.description, c.description)
+
+    def test_skipped_data(self):
+        self.soft = SOFTFile(os.path.join(self.data_dir, 'GDS1099.soft'), skip_data=True)
+
+        self.assertEqual(len(self.soft.data), 0)
